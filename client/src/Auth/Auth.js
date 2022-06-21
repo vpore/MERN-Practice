304647d5972c9c4e5efb402741fa7e9a6c42ad23
Vaibhavi <porevaibhavi5@gmail.com>
2022-06-21 22:43:05 +0530

    new: authentication

---
+++ client/src/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const initialState = { name: '', email: '', password: '', confirmPassword: ''};
+
+const Auth = () => {
+
+    return(
+        <>
+            
+        </>
+    );
+}
+
+export default Auth;
